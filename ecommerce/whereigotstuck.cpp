001889c642aea7bc3c3bfce25ac922374ba2172d
Sharan S poojari <117771260+Sharanpoojarissp@users.noreply.github.com>
2023-07-10 01:56:01 +0530

    Add files via upload

---
+++ ecommerce/whereigotstuck.cpp
@@ -0,0 +1,14 @@
+// list ke ander list dala tha 
+// how to add a placeholder in the left most side 
+// allign items in a single line 
+// removing the line from the items : text deco is none for the a tag 
+
+//! till now : i have made the navbar and all the above were my mistakes and solutions to the mistakes :
+
+//* now i will create a divs 
+// peoblem is zoomed in pictures whixh need to get fixed < despite of keeping the backimg as cover : 
+// done with the div part now need to create a footer 
+
+// p tag --> ext-align: justify; to justify content written or lorem 
+
+//* media queries need to be used so learn it first :
